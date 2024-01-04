ccd573284fbe6261f3269f82de408df63936985b
muhammedaslamshah <muhammedaslamshah700@gmail.com>
2024-01-05 01:32:02 +0530

    3rd commit | updated

--- script.js
+++ script.js
@@ -3,31 +3,22 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("views"));
 
-
-const emailDB = "superuser@gmail.com"
+const emailDB = "superuser@gmail.com";
 const passwordDB = "123";
 
-
-
 app.post("/login", (req, res) => {
+  const { email, password } = req.body;
 
-    const { email, password } = req.body;
-
-    if(email === emailDB && password === passwordDB){
-        res.send("Login Successful");
-    }else{
-        res.send("login faild");
-    }
-
+  if (email === emailDB && password === passwordDB) {
+    res.send("Login Successful");
+  } else {
+    res.send("login faild");
+  }
 });
 
-
-
-
 const PORT = process.env.PORT || 3001;
-
 
 app.listen(PORT);
--- views/js/animate.js
+++ views/js/animate.js
@@ -3,26 +3,26 @@ const formContainer = document.querySelector(".login-container");
 const loginContainer = document.querySelector(".login");
 const profile = document.querySelector("img");
 
-
 profile.onclick = () => {
-   requestAnimationFrame(() => {
-      
-      if (!loginContainer.classList.contains("hide-login") || !formContainer.classList.contains("hide-form") || !section.classList.contains("hide-section")) {
-         
-         loginContainer.classList.add("hide-login");
-         formContainer.classList.add("hide-form");
-         section.classList.add("hide-section");
-         
-        
-         setTimeout(() => {
-            loginContainer.classList.add("hide-login");
-            formContainer.classList.add("hide-form");
-            section.classList.add("hide-section");
-         }, 1000);
-      } else {
-         loginContainer.classList.remove("hide-login");
-         formContainer.classList.remove("hide-form");
-         section.classList.remove("hide-section");
-      }
-   });
+  requestAnimationFrame(() => {
+    if (
+      !loginContainer.classList.contains("hide-login") ||
+      !formContainer.classList.contains("hide-form") ||
+      !section.classList.contains("hide-section")
+    ) {
+      loginContainer.classList.add("hide-login");
+      formContainer.classList.add("hide-form");
+      section.classList.add("hide-section");
+
+      setTimeout(() => {
+        loginContainer.classList.add("hide-login");
+        formContainer.classList.add("hide-form");
+        section.classList.add("hide-section");
+      }, 1000);
+    } else {
+      loginContainer.classList.remove("hide-login");
+      formContainer.classList.remove("hide-form");
+      section.classList.remove("hide-section");
+    }
+  });
 };
