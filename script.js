3a2746004204df1359c48abcdbfc349fd1b3fd96
muhammedaslamshah <muhammedaslamshah700@gmail.com>
2024-01-05 01:27:08 +0530

    2nd commit | updated like hardcore

--- script.js
+++ script.js
@@ -2,6 +2,31 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static("views"));
+
+
+const emailDB = "superuser@gmail.com"
+const passwordDB = "123";
+
+
+
+app.post("/login", (req, res) => {
+
+    const { email, password } = req.body;
+
+    if(email === emailDB && password === passwordDB){
+        res.send("Login Successful");
+    }else{
+        res.send("login faild");
+    }
+
+});
+
+
+
+
 const PORT = process.env.PORT || 3001;
 
 
